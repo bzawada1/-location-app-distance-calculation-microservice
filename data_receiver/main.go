2d4de5308529ea6e2608651a7f4b76c0f347aa1b
Bartlomiej Zawada <b.zawada@andersenlab.com>
2024-05-02 18:05:24 +0200

    Add binaries to gitignore

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/bzawada1/location-app-obu-service/types"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/gorilla/websocket"
 )
 
+var kafkaTopic = "obudata"
+
 func main() {
-	receiver := NewDataReceiver()
+	// Delivery report handler for produced messages
+
+	receiver, err := NewDataReceiver()
+	if err != nil {
+		log.Fatal(err)
+	}
 	http.HandleFunc("/ws", receiver.handleWs)
 	http.ListenAndServe(":30000", nil)
 }
@@ -18,13 +27,46 @@ func main() {
 type DataReceiver struct {
 	msg  chan types.OBUData
 	conn *websocket.Conn
+	prod *kafka.Producer
 }
 
-func NewDataReceiver() *DataReceiver {
-	return &DataReceiver{
-		msg: make(chan types.OBUData, 128),
+func NewDataReceiver() (*DataReceiver, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		return nil, err
 	}
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+	return &DataReceiver{
+		prod: p,
+	}, nil
 }
+
+func (dataReceiver *DataReceiver) produceData(data types.OBUData) error {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	err = dataReceiver.prod.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &kafkaTopic,
+			Partition: kafka.PartitionAny,
+		},
+		Value: b,
+	}, nil)
+	return err
+}
+
 func (dataReceiver *DataReceiver) handleWs(w http.ResponseWriter, r *http.Request) {
 	u := websocket.Upgrader{
 		ReadBufferSize:  1028,
@@ -48,6 +90,8 @@ func (dataReceiver *DataReceiver) wsReceiveLoop() {
 		}
 		fmt.Println("received OBU data: ", data.Long)
 
-		dataReceiver.msg <- data
+		if err := dataReceiver.produceData(data); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
